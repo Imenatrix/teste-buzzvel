992478fc768e223f5b09623b3225e05f1f941222
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-03-25 23:42:45 -0300

    Add tests for slug generation

---
+++ tests/Feature/SlugTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Lib\Slug;
+use App\Models\Cartao;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SlugTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_can_convert_name_to_slug(): void
+    {
+        $result = Slug::nameToSlug('Imena Gomes do Rosario');
+        $this->assertEquals('imena_gomes_do_rosario', $result);
+    }
+
+    public function test_can_count_repeated_names(): void
+    {
+        Cartao::factory()->create(['nome' => 'Pedro Silva']);
+        $result = Slug::generateSlugSuffix('Pedro Silva');
+        $this->assertEquals('1', $result);
+    }
+
+    public function test_can_create_slug(): void
+    {
+        $cartao = new Cartao;
+        $cartao->nome = 'Gabriel Crisanto';
+        $result = Slug::generateSlug($cartao);
+        $this->assertEquals('gabriel_crisanto', $result);
+    }
+
+    public function test_can_create_slug_with_suffix(): void
+    {
+        Cartao::factory()->create(['nome' => 'Rafael Silva', 'slug' => 'rafael_silva']);
+        Cartao::factory()->create(['nome' => 'Rafael Silva', 'slug' => 'rafael_silva_1']);
+        $cartao = Cartao::factory()->make(['nome' => 'Rafael Silva']);
+        $result = Slug::generateSlug($cartao);
+        $this->assertEquals('rafael_silva_2', $result);
+    }
+}
